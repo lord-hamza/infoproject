84286cc0c70eedb9c2e8452bc87c55c0343e6cfb
lord-hamza <115451025+lord-hamza@users.noreply.github.com>
2023-01-22 00:53:53 +0500

    Update and rename py-code-decoder.py to py-code-decoder.php

---
+++ py-code-decoder.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+snykSecurity snykInstallation: 'Snyk-Info', snykTokenId: '3166c47b-033c-49ab-b212-e84043c262c4'
+?>
+
+</body>
+</html>
